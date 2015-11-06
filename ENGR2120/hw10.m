a94fb9e7e8358a993e08e53d12683d24f73a50cb
briandwendt <brian@aeropunk.com>
2015-11-05 22:47:41 -0600

    Added some Circuits HW.

--- ENGR2120/hw10.m
+++ ENGR2120/hw10.m
@@ -5,13 +5,32 @@
 %
 clc; clear; format compact;
 
-% the decay function
-t = linspace(0,1);
-iR = 1 + 0.6 * exp(-t/0.096);
+% Current at t<0
+t0 = linspace(-1,0);
+i0 = 1 + 0*t0;
 
-% the grapherooni
-plot(t,iR,'LineWidth',4);
-grid on;
-xlim([-0.1 1]);
-ylim([0 1.7]);
-PlotAxisAtOrigin(t,iR);
+% Current at t=0
+x = [0 0];
+y = [1 1.6];
+
+% Current decay at t>0
+t1 = linspace(0,1);
+iR = 1 + 0.6 * exp(-t1/0.096);
+
+% Create figure
+figure('Position', [900, 900, 600, 600]);
+hold on; grid on;
+
+% Graph it up
+plot(x,y,'m','LineWidth',5)
+plot(t0,i0,'m','LineWidth',5);
+plot(t1,iR,'m','LineWidth',5);
+
+% Set axes limits
+xlim([-0.1 0.7]);
+ylim([-0.1 1.8]);
+
+% Move axes to origin
+ax = gca;
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
